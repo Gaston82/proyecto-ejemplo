bd9e94591f4cb9b075374ae26b47e6d88c4806aa
Gaston82 <cabezaartaza@hotmail.com>
2022-04-27 01:57:07 +0200

    add delete,post,get,put examples

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const express = require("express");
 const { port } = require("./config");
 
 const app = express();
+// Middlaware para usar un body en  json
+app.use(express.json());
+
+let users = [];
 
 app.get("/", (req, res) => {
-  res.json({ hola: "mundo" });
+  return res.json(users);
+});
+
+app.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  return res.json(users);
+});
+
+//Revisar filter
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  users = users.filter((user) => id !== user.id);
+  return res.json(users);
+});
+
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  users = users.map((user) => (user.id === id ? body : user));
+
+  return res.json(users);
 });
 
 app.listen(port, () => {
